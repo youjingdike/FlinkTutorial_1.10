2f2ecce5a3746cbbffbc2ae17903b0e503e211b6
dike <82329839@QQ.com>
2020-08-03 14:28:30 +0800

    提交代码

--- src/main/scala/com/atguigu/apitest/tabletest/udftest/TableFunctionTest.scala
+++ src/main/scala/com/atguigu/apitest/tabletest/udftest/TableFunctionTest.scala
@@ -28,7 +28,7 @@ object TableFunctionTest {
     // 创建表执行环境
     val tableEnv = StreamTableEnvironment.create(env)
 
-    val inputStream: DataStream[String] = env.readTextFile("D:\\Projects\\BigData\\FlinkTutorial\\src\\main\\resources\\sensor.txt")
+    val inputStream: DataStream[String] = env.readTextFile("D:\\code\\FlinkTutorial_1.10\\src\\main\\resources\\sensor.txt")
     //    val inputStream: DataStream[String] = env.socketTextStream("localhost", 7777)
 
     // map成样例类类型
