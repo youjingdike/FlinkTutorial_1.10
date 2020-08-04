0fd9fe3cb9e8d24777f3bcc4e216b7ecbe26645e
dike <82329839@QQ.com>
2020-08-04 11:32:25 +0800

    提交代码

--- src/main/java/com/xq/apitest/TransformTest.java
+++ src/main/java/com/xq/apitest/TransformTest.java
@@ -4,9 +4,19 @@ import com.xq.apitest.pojo.SensorReading1;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransformTest {
     public static void main(String[] args) throws Exception {
@@ -28,13 +38,62 @@ public class TransformTest {
         //如果是某个key的第一条数据，不执行该方法
         SingleOutputStreamOperator<SensorReading1> reduce = dataStream.keyBy("id")
                 .reduce((ReduceFunction<SensorReading1>) (cur, newData) -> {
-                    System.out.println("cur:"+cur);
-                    System.out.println("new:"+newData);
+//                    System.out.println("cur:"+cur);
+//                    System.out.println("new:"+newData);
                     return new SensorReading1(cur.getId(), newData.getTimestamp() + 1, Math.min(cur.getTemperature(), newData.getTemperature()));
                 });
 
+        // 4. 分流操作，split/select，以30度为界划分高低温流
+        SplitStream<SensorReading1> splitStream = dataStream.split(new OutputSelector<SensorReading1>() {
+            @Override
+            public Iterable<String> select(SensorReading1 value) {
+                List<String> output = new ArrayList<String>();
+                if (value.getTemperature() > 30) {
+                    output.add("high");
+                } else {
+                    output.add("low");
+                }
+                return output;
+            }
+        });
+        DataStream<SensorReading1> highStream = splitStream.select("high");
+        DataStream<SensorReading1> lowStream = splitStream.select("low");
+        DataStream<SensorReading1> allStream = splitStream.select("high", "low");
+
+        // 5. 合流操作，connect/comap
+        SingleOutputStreamOperator<Tuple2<String, Double>> highWarningStream = highStream.map(new MapFunction<SensorReading1, Tuple2<String, Double>>() {
+            @Override
+            public Tuple2<String, Double> map(SensorReading1 value) throws Exception {
+                return new Tuple2<>(value.getId(), value.getTemperature());
+            }
+        });
+
+        ConnectedStreams<Tuple2<String, Double>, SensorReading1> connectedStream = highWarningStream.connect(lowStream);
+        SingleOutputStreamOperator<Tuple3<String, Double, String>> coMapStream = connectedStream.map(new CoMapFunction<Tuple2<String, Double>, SensorReading1, Tuple3<String, Double, String>>() {
+            @Override
+            public Tuple3<String, Double, String> map1(Tuple2<String, Double> value) throws Exception {
+                return new Tuple3<>(value.f0, value.f1, "warning");
+            }
+
+            @Override
+            public Tuple3<String, Double, String> map2(SensorReading1 value) throws Exception {
+                return new Tuple3<>(value.getId(), value.getTemperature(), "normal");
+            }
+        });
+        /*Connect 与 Union 区别：
+        * 1． Union 之前两个流的类型必须是一样， Connect 可以不一样，在之后的 coMap中再去调整成为一样的。
+        * 2. Connect 只能操作两个流， Union 可以操作多个。
+        */
+        DataStream<SensorReading1> unionStream = highStream.union(lowStream);
 //        minby.print("minby");
-        reduce.print("reduce");
+//        reduce.print("reduce");
+        /*highStream.print("high");
+        lowStream.print("low");
+        allStream.print("all");*/
+//        coMapStream.print("coMapStream");
+//        unionStream.print("union");
+
+
         env.execute();
 
 
