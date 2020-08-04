25fcababf5adfe4e3d1819b2cc2814dba3eb8547
dike <82329839@QQ.com>
2020-08-04 10:10:31 +0800

    提交代码

--- src/main/java/com/xq/apitest/TransformTest.java
+++ src/main/java/com/xq/apitest/TransformTest.java
@@ -2,6 +2,7 @@ package com.xq.apitest;
 
 import com.xq.apitest.pojo.SensorReading1;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -11,18 +12,29 @@ public class TransformTest {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
+        // 从文件读取数据
+        DataStreamSource<String> inputStream = env.readTextFile("D:\\code\\FlinkTutorial_1.10\\src\\main\\resources\\sensor.txt");
 
-        DataStreamSource<String> source = env.readTextFile("D:\\code\\FlinkTutorial_1.10\\src\\main\\resources\\sensor.txt");
-        SingleOutputStreamOperator<SensorReading1> out = source.map((MapFunction<String, SensorReading1>) value -> {
+        // 1. 基本转换操作：map成样例类类型
+        SingleOutputStreamOperator<SensorReading1> dataStream = inputStream.map((MapFunction<String, SensorReading1>) value -> {
             String[] split = value.split(",");
             return new SensorReading1(split[0].trim(), Long.parseLong(split[1].trim()), Double.parseDouble(split[2].trim()));
         });
-
-        SingleOutputStreamOperator<SensorReading1> minby = out.keyBy((KeySelector<SensorReading1, String>) value -> value.getId())
+        // 2. 聚合操作，首先按照id做分组，然后取当前id的最小温度
+        SingleOutputStreamOperator<SensorReading1> minby = dataStream.keyBy((KeySelector<SensorReading1, String>) value -> value.getId())
                 .minBy("temperature");
 
-        minby.print("minby");
+        // 3. 复杂聚合操作，reduce，得到当前id最小的温度值，以及最新的时间戳+1
+        //如果是某个key的第一条数据，不执行该方法
+        SingleOutputStreamOperator<SensorReading1> reduce = dataStream.keyBy("id")
+                .reduce((ReduceFunction<SensorReading1>) (cur, newData) -> {
+                    System.out.println("cur:"+cur);
+                    System.out.println("new:"+newData);
+                    return new SensorReading1(cur.getId(), newData.getTimestamp() + 1, Math.min(cur.getTemperature(), newData.getTemperature()));
+                });
 
+//        minby.print("minby");
+        reduce.print("reduce");
         env.execute();
 
 
